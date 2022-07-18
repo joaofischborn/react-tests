4816aefa3083da0c78c45e93295406738b57854b
João Vitor <joaovitorfischborn@icloud.com>
2022-07-18 16:44:10 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Teste o componente App', () => {
+  test('Teste se o topo da aplicação contém um conjunto de links de navegação', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    expect(linkHome).toBeInTheDocument();
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout).toBeInTheDocument();
+    const linkFavorite = screen.getByRole('link', { name: /favorite/i });
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});
