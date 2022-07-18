b8b6191c988fe01d77a94611e944b1f1c81f7aa2
João Vitor <joaovitorfischborn@icloud.com>
2022-07-18 18:28:43 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+
+describe('Teste o componente Pokemon', () => {
+  test('Teste se é renderizado um card com as informações de um pokémon', () => {
+    render(<MemoryRouter><FavoritePokemons /></MemoryRouter>);
+    const noFavoritePokemon = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavoritePokemon).toBeInTheDocument();
+  });
+});
